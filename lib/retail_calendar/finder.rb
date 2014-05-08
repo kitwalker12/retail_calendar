f06e172d17e242ffa311280df08752950a5d1fce
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2014-05-08 11:09:13 -0700

    Figure out how to calculate Start of Retail Year

--- lib/retail_calendar/finder.rb
+++ lib/retail_calendar/finder.rb
@@ -2,11 +2,11 @@ require "active_support/all"
 
 module RetailCalendar
   class Finder
-    attr_accessor :timezone
-    @timezone
+    attr_accessor :offset
+    @offset
 
-    def initialize(timezone = 'UTC')
-      @timezone = timezone
+    def initialize(offset = '+0000')
+      @offset = offset
     end
 
     def week(year, period, day)
@@ -36,11 +36,34 @@ module RetailCalendar
     end
 
     def month_offset_from_year_start(month)
+      month = month.to_i
       month == 1 ? 11 : month - 2
     end
 
+    def weeks_in_month(month)
+      month = month.to_i
+      month % 3 == 0 ? 5 : 4
+    end
+
     def start_time_for_year(year)
-      #???
+      year = year.to_i
+      jan31 = DateTime.new(year,1,31).change(:offset => @offset)
+      jan31day = jan31.cwday
+      if jan31day < 3
+        start = jan31 - jan31day
+      elsif jan31day < 7
+        start = jan31 + (7 - jan31day)
+      elsif jan31day == 7
+        start = jan31
+      end
+      start
+    end
+
+    def year_has_extra_week?(year)
+      year = year.to_i
+      expected = start_time_for_year(year) + 364
+      actual = start_time_for_year(year + 1)
+      actual > expected ? true : false
     end
   end
 end
