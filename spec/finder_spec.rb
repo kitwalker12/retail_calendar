8b91f8c54a182468b4000c06f3f87cebb976f15f
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2014-05-08 12:37:09 -0700

    Add More comprehensive tests for core functions

--- spec/finder_spec.rb
+++ spec/finder_spec.rb
@@ -6,32 +6,59 @@ describe RetailCalendar::Finder do
   describe "Core functions" do
     it "should return correct week start and end dates" do
       res = cal.week(2014, 1, 1)
-      expect(res.start).to eq(DateTime.new(2014,2,2))
-      expect(res.end).to eq(DateTime.new(2014,2,8))
+      expect(res.start_date).to eq(DateTime.new(2014,2,2))
+      expect(res.end_date).to eq(DateTime.new(2014,2,8))
+      res = cal.week(2014, 5, 2)
+      expect(res.start_date).to eq(DateTime.new(2014,6,8))
+      expect(res.end_date).to eq(DateTime.new(2014,6,14))
+      res = cal.week(2014, 12, 3)
+      expect(res.start_date).to eq(DateTime.new(2015,1,18))
+      expect(res.end_date).to eq(DateTime.new(2015,1,24))
     end
 
     it "should return correct period start and end dates" do
       res = cal.period(2014, 1)
-      expect(res.start).to eq(DateTime.new(2014,2,2))
-      expect(res.end).to eq(DateTime.new(2014,3,1))
+      expect(res.start_date).to eq(DateTime.new(2014,2,2))
+      expect(res.end_date).to eq(DateTime.new(2014,3,1))
+      res = cal.period(2014, 4)
+      expect(res.start_date).to eq(DateTime.new(2014,5,4))
+      expect(res.end_date).to eq(DateTime.new(2014,5,31))
+      res = cal.period(2014, 8)
+      expect(res.start_date).to eq(DateTime.new(2014,8,31))
+      expect(res.end_date).to eq(DateTime.new(2014,10,4))
+      res = cal.period(2014, 12)
+      expect(res.start_date).to eq(DateTime.new(2015,1,4))
+      expect(res.end_date).to eq(DateTime.new(2015,1,31))
     end
 
     it "should return correct quarter start and end dates" do
       res = cal.quarter(2014, 1)
-      expect(res.start).to eq(DateTime.new(2014,2,2))
-      expect(res.end).to eq(DateTime.new(2014,5,3))
+      expect(res.start_date).to eq(DateTime.new(2014,2,2))
+      expect(res.end_date).to eq(DateTime.new(2014,5,3))
+      res = cal.quarter(2014, 2)
+      expect(res.start_date).to eq(DateTime.new(2014,5,4))
+      expect(res.end_date).to eq(DateTime.new(2014,8,2))
+      res = cal.quarter(2014, 3)
+      expect(res.start_date).to eq(DateTime.new(2014,8,3))
+      expect(res.end_date).to eq(DateTime.new(2014,11,1))
+      res = cal.quarter(2014, 4)
+      expect(res.start_date).to eq(DateTime.new(2014,11,2))
+      expect(res.end_date).to eq(DateTime.new(2015,1,31))
     end
 
     it "should return correct season start and end dates" do
       res = cal.season(2014, 1)
-      expect(res.start).to eq(DateTime.new(2014,2,2))
-      expect(res.end).to eq(DateTime.new(2014,8,2))
+      expect(res.start_date).to eq(DateTime.new(2014,2,2))
+      expect(res.end_date).to eq(DateTime.new(2014,8,2))
+      res = cal.season(2014, 2)
+      expect(res.start_date).to eq(DateTime.new(2014,8,3))
+      expect(res.end_date).to eq(DateTime.new(2015,1,31))
     end
 
     it "should return correct year start and end dates" do
       res = cal.year(2014)
-      expect(res.start).to eq(DateTime.new(2014,2,2))
-      expect(res.end).to eq(DateTime.new(2015,2,31))
+      expect(res.start_date).to eq(DateTime.new(2014,2,2))
+      expect(res.end_date).to eq(DateTime.new(2015,1,31))
     end
   end
 
